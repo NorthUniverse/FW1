f1ab923b69f92c8e3e494501bf236a1701d78bc4
Dennis Richard <dennis.richard@bloomingdales.com>
2018-09-26 17:24:12 -0400

    made quit = true

---
+++ src/Jukebox/Song.java
@@ -0,0 +1,12 @@
+package Jukebox;
+
+public class Song {
+    private String name;
+    private int duration;
+
+    public Song(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+
+}
