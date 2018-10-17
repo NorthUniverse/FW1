6537686e08158b21d66370c26de05ff4bba60edc
NorthUniverse <dennis.roshan@gmail.com>
2018-10-16 22:06:50 -0400

    made to run

---
+++ src/Interface/Saveable.java
@@ -0,0 +1,44 @@
+package Interface;
+
+import java.util.ArrayList;
+
+public class Saveable implements ISaveable{
+    private ArrayList<String> myArrayList;
+    private int index;
+
+    public Saveable() {
+        myArrayList = new ArrayList<String>();
+        index = 0;
+    }
+
+    @Override
+    public void inputDataToArrayList(String data) {
+        myArrayList.add(index,data);
+        index++;
+    }
+
+    @Override
+    public void printDataFromArrayList() {
+        if(index > 0) {
+            for(int i = 0; i < index; i++) {
+                System.out.println((i+1) + ". -> " + myArrayList.get(i));
+            }
+        }
+        else {
+            System.out.println("The array list is empty!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        if(index > 0) {
+            for(int i = 0; i < index; i++) {
+                System.out.println((i+1) + ". -> " + myArrayList.get(i));
+            }
+        }
+        else {
+            System.out.println("The array list is empty!");
+        }
+        return null;
+    }
+}
