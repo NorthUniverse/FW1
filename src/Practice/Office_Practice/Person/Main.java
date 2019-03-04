8ddbe3729d3830c180102b7ea3e1620572053f7c
Dennis Richard <dennis.richard@bloomingdales.com>
2019-03-04 18:44:41 -0500

    Commit 3/4/2019

--- src/Practice/Office_Practice/Person/Main.java
+++ src/Practice/Office_Practice/Person/Main.java
@@ -1,19 +1,43 @@
 package Practice.Office_Practice.Person;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
-	Person Sankeerth = new Person("Sankeerth","Kakarna", 28);
-	Person Richard = new Person("Richard","McLeggon", 40);
-	Person Dennis = new Person("Dennis","Richard", 30);
-	Person Mizan = new Person("Mizan","Mossain", 27);
-	Person John = new Person("John","Cunningham", 32);
-	Person Lisa = new Person("Lisa","Caton", 30);
+	public static void main(String[] args) {
+		Person sankeerth = new Person("Sankeerth","Kakarna", 28);
+		Person richard = new Person("Richard","McLeggon", 40);
+		Person dennis = new Person("Dennis","Richard", 30);
+		Person mizan = new Person("Mizan","Mossain", 27);
+		Person john = new Person("John","Cunningham", 32);
+		Person lisa = new Person("Lisa","Caton", 30);
 
-	List<Person> persons = new ArrayList<>();
+		List<Person> persons = new ArrayList<>();
 
-	persons.add(Sankeerth);
+		persons.add(sankeerth);
+		persons.add(richard);
+		persons.add(dennis);
+		persons.add(mizan);
+		persons.add(john);
+		persons.add(lisa);
+
+		printPersons(persons);
+		sortPersonsLastName(persons);
+		System.out.println(" ");
+		System.out.println("Persons after sorting using Last Name: ");
+		printPersons(persons);
+	}
+
+	public static void printPersons(List<Person> persons) {
+		for(Person person : persons) {
+			System.out.println(person.getPerson());
+		}
+	}
+
+	public static void sortPersonsLastName(List<Person> persons) {
+		Collections.sort(persons);
+	}
 
 }
