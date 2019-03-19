58ada817624a2988973eec3cc1bd5c44245cae15
Dennis Richard <dennis.richard@bloomingdales.com>
2019-03-19 18:47:15 -0400

    Commit 3/19/2019

--- src/Practice/My_Practice_Coding_Exercise/DupArray.java
+++ src/Practice/My_Practice_Coding_Exercise/DupArray.java
@@ -1,10 +1,13 @@
 package Practice.My_Practice_Coding_Exercise;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DupArray {
 
     public static void main(String[] args) {
-        int[] myArray = {1,2,2,3,4,4};
-        printDups(myArray);
+        int[] myArray = {4,4,4};
+		printDupsWithOccurance(myArray);
     }
 
     public static void printDups(int[] passedArray) {
@@ -16,5 +19,26 @@ public class DupArray {
             }
         }
     }
+    public static void printDupsWithOccurance(int[] passedArray) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < passedArray.length; i++) {
+			int noOfOccurance = 1;
+            for(int j = i+1; j < passedArray.length; j++) {
+                if(passedArray[i] == passedArray[j]) {
+                    noOfOccurance++;
+                    if(map.containsKey(passedArray[j])) {
+                    	noOfOccurance++;
+					}
+                    else {
+						map.put(passedArray[i], noOfOccurance);
+					}
+                }
+            }
+        }
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			System.out.println("Element " + entry.getKey() + " with no. of occurance " + entry.getValue());
+		}
+    }
+
 
 }
