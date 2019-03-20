b098d0cd59b01def6a381e6373f5535ca139d5a5
Dennis Richard <dennis.richard@bloomingdales.com>
2019-03-20 13:54:12 -0400

    Commit 3/20/2019

--- src/Practice/My_Practice_Coding_Exercise/Fibonacci.java
+++ src/Practice/My_Practice_Coding_Exercise/Fibonacci.java
@@ -2,21 +2,34 @@ package Practice.My_Practice_Coding_Exercise;
 
 public class Fibonacci {
     public static void main(String[] args) {
+
         int n = 10;
-        System.out.println("0" + " " + "1");
-        fib(n-2);
+        System.out.println("Fibonacci number upto " + n + " using recursion: ");
+        System.out.println(fibonacci(n));
+
+        System.out.println("Fibonacci series upto " + n +" numbers : ");
+        for(int i=1; i<=n; i++) {
+            System.out.print(fibonacci2(i) +" ");
+        }
     }
 
-    private static void fib(int n) {
-        if(n > 0) {
-            int n1 = 0;
-            int n2 = 1;
-            int n3;
-            n3 = n1 + n2;
-            n1 = n2;
-            n2 = n3;
-            System.out.println(" " + n3);
-            fib(n-1);
+    private static int fibonacci(int n) {
+        if(n == 1 || n == 2){
+            return 1;
         }
+        return fibonacci(n-1) + fibonacci(n -2);
+    }
+
+    private static int fibonacci2(int n) {
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        int fibo1=1, fibo2=1, fibonacci=1;
+        for(int i= 3; i<= n; i++){
+            fibonacci = fibo1 + fibo2;
+            fibo1 = fibo2;
+            fibo2 = fibonacci;
+        }
+        return fibonacci;
     }
 }
