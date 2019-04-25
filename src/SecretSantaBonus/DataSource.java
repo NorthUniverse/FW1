d1912efe48e7b44f76dde5a8b830780447099ac9
Dennis Richard <dennis.richard@bloomingdales.com>
2019-04-25 19:50:02 -0400

    Commit 4/25/2019

--- src/SecretSantaBonus/DataSource.java
+++ src/SecretSantaBonus/DataSource.java
@@ -1,5 +1,20 @@
 package SecretSantaBonus;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DataSource {
+	public static void main(String[] args) {
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:c:\\secretSanta.db");
+			Statement statement = conn.createStatement();
+			statement.execute("CREATE TABLE IS NOT EXISTS person " +
+					               " (name TEXT, exclusuions TEXT, secretSantaFor TEXT)");
+		} catch (SQLException e) {
+			System.out.println("SQL Exception: " + e.getMessage());
+		}
+	}
 
 }
