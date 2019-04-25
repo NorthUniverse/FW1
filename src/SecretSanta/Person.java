c6246d6a21b883026a36b0f2e3636130b9377b5a
NorthUniverse <dennis.roshan@gmail.com>
2019-04-24 20:50:28 -0400

    Commit 4/24/2019

--- src/SecretSanta/Person.java
+++ src/SecretSanta/Person.java
@@ -8,12 +8,10 @@ public class Person {
 	private String name;
 	private List<String> exclusions; //List for exclusions(immediate family menbers)
 	private List<String> secrectSantaFor;
-	private List<String> canGiftToList;
 
 	public Person(String name) {
 		this.name = name;
 		this.exclusions = new ArrayList<>();
-		this.canGiftToList = new ArrayList<>();
 		this.secrectSantaFor = new ArrayList<>();
 	}
 
@@ -31,14 +29,6 @@ public class Person {
 
 	public void setExclusions(String name) {
 		this.exclusions.add(name);
-	}
-
-	public List<String> getCanGiftToList() {
-		return canGiftToList;
-	}
-
-	public void setCanGiftToList(String name) {
-		this.canGiftToList.add(name);
 	}
 
 	public List<String> getSecrectSantaFor() {
