a95ebdb0bcd814b15b5011651a7acc803b1b954e
Dennis Richard <dennis.richard@bloomingdales.com>
2019-04-26 17:36:29 -0400

    Commit 4/25/2019

--- src/SecretSantaBonus/DataSource.java
+++ src/SecretSantaBonus/DataSource.java
@@ -1,24 +1,48 @@
 package SecretSantaBonus;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import oracle.jrockit.jfr.tools.ConCatRepository;
+import org.sqlite.core.DB;
+
+import java.sql.*;
 
 public class DataSource {
+
+	public static final String DB_NAME = "secretSanta.db";
+	public static final String CONNECTION_STRING = "jdbc:sqlite:c:\\FW1\\src\\SecretSantaBonus\\" + DB_NAME;
+
+	public static final String TABLE_PERSONS = "persons";
+
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_EXCLUSIONS = "exclusions";
+	public static final String COLUMN_SECRETSANTAFOR = "secretSantaFor";
+
 	public static void main(String[] args) {
 		try {
-			Connection conn = DriverManager.getConnection("jdbc:sqlite:c:\\secretSanta.db");
+			Connection conn = DriverManager.getConnection(CONNECTION_STRING);
 			Statement statement = conn.createStatement();
-			statement.execute("CREATE TABLE IS NOT EXISTS person " +
-					               " (name TEXT, exclusuions TEXT, secretSantaFor TEXT)");
-//			statement.execute("INSERT INTO person (name, exclusions, secretSantaFor) " +
-//					               "VALUES('Jon','Sumo','Sam')");
-			statement.execute("CREATE TABLE IS NOT EXISTS person " +
-					" (name TEXT, exclusuions TEXT, secretSantaFor TEXT)");
-			statement.execute("CREATE TABLE IS NOT EXISTS person " +
-					" (name TEXT, exclusuions TEXT, secretSantaFor TEXT)");
+			statement.execute("DROP TABLE IF EXISTS " + TABLE_PERSONS);
+			statement.execute("CREATE TABLE IF NOT EXISTS persons " +
+					               " (name TEXT, exclusions TEXT, secretSantaFor TEXT)");
+//			statement.execute("INSERT INTO persons (name, exclusions, secretSantaFor) " +
+//					               "VALUES('Dennis','Oishi','Omi')");
+//			statement.execute("INSERT INTO persons (name, exclusions, secretSantaFor) " +
+//					"VALUES('Oishi','Dennis','Rahul')");
+//			statement.execute("INSERT INTO persons (name, exclusions, secretSantaFor) " +
+//					"VALUES('Rahul','','Dennis')");
+//			statement.execute("UPDATE persons SET secretSantaFor = 'stringList' WHERE name = 'query name'");
+//			statement.execute("DELETE FROM persons WHERE name = 'query name'");
 
+//			statement.execute("SELECT * FROM persons");
+//			ResultSet results = statement.getResultSet();
+
+			ResultSet results = statement.executeQuery("SELECT * FROM persons");
+
+			while(results.next()) {
+				System.out.println(results.getString("name") + " " +
+									results.getString("exclusions") + " " +
+									results.getString("secretSantaFor"));
+			}
+			results.close();
 			statement.close();
 			conn.close();
 		} catch (SQLException e) {
