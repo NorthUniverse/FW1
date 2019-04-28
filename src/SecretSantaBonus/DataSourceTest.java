fa2962f1e76caa19fdfcae61c54fcfb6bdbd6914
NorthUniverse <dennis.roshan@gmail.com>
2019-04-28 13:42:09 -0400

    Commit 4/28/2019

---
+++ src/SecretSantaBonus/DataSourceTest.java
@@ -0,0 +1,54 @@
+package SecretSantaBonus;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataSourceTest {
+
+    @Test
+    public void closeConnection() {
+        DataSource.openConnection();
+        
+    }
+
+    @Test
+    public void initialize() {
+    }
+
+    @Test
+    public void queryNameColumn() {
+    }
+
+    @Test
+    public void updateNameColumn() {
+    }
+
+    @Test
+    public void saveToDB() {
+    }
+
+    @Test
+    public void saveToDB1() {
+    }
+
+    @Test
+    public void removeFromDB() {
+    }
+
+    @Test
+    public void readFromDB() {
+    }
+
+    @Test
+    public void addExclusionsToDB() {
+    }
+
+    @Test
+    public void saveSecretSantaToDB() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
